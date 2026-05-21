4546e8d8302a8ea999106fa27d498f502eb4d140
Steven Dürrenmatt <steven.durrenmatt@gmail.com>
2026-05-22 00:22:22 +0200

    Drop public for main methods

--- examples/rest/src/main/java/com/durrenmatt/camel/examples/rest/RestApplication.java
+++ examples/rest/src/main/java/com/durrenmatt/camel/examples/rest/RestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
 }
